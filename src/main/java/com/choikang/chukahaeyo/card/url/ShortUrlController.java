bed66676612cd1e0af9f8afdefb89a61ea1cfea0
wing-beat <wingbeat1115@gmail.com>
2024-09-09 12:19:00 +0900

    fix: Reset AWS Information

--- src/main/java/com/choikang/chukahaeyo/card/url/ShortUrlController.java
+++ src/main/java/com/choikang/chukahaeyo/card/url/ShortUrlController.java
@@ -18,7 +18,8 @@ public class ShortUrlController {
     @GetMapping("/origin")
     @ResponseBody
     public String url() {
-        return "http://3.36.97.132:9090/payments/success"; // test용으로 URL을 미리 넣어둔 것. 추후 사용자별 카드 URL로 변경 예정
+        return "http://13.125.165.147:9090/payments/success"; // test용으로 URL을 미리 넣어둔 것. 추후 사용자별 카드 URL로 변경 예정
+//        return "http://3.36.97.132:9090/payments/success"; // test용으로 URL을 미리 넣어둔 것. 추후 사용자별 카드 URL로 변경 예정
     }
 
     @GetMapping("/shorts")
